9f60f2474477035dee49fa7ea9a4a4ef1f201cb0
david <dav94sal@gmail.com>
2024-07-18 10:58:03 -0500

    Add User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -7,6 +7,7 @@ const { User } = require('../../db/models');
 
 const router = express.Router();
 
+// Log in
 router.post('/', async (req, res, next) => {
   const { credential, password } = req.body;
 
@@ -36,6 +37,12 @@ router.post('/', async (req, res, next) => {
   await setTokenCookie(res, safeUser);
 
   return res.json ({user: safeUser})
+})
+
+// Log out
+router.delete('/', async (req, res, next) => {
+  res.clearCookie('token');
+  return res.json({message: 'success'});
 })
 
 module.exports = router;
