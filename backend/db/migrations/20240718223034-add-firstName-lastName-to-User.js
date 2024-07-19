3245a3d147d72bfd75f8fa878a5d00fcbcfc369d
david <dav94sal@gmail.com>
2024-07-18 19:08:48 -0500

    Add production environment code for migration

--- backend/db/migrations/20240718223034-add-firstName-lastName-to-User.js
+++ backend/db/migrations/20240718223034-add-firstName-lastName-to-User.js
@@ -1,5 +1,10 @@
 'use strict';
 
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+};
+
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
@@ -8,10 +13,11 @@ module.exports = {
     })
     await queryInterface.addColumn('Users', 'lastName', {
       type: Sequelize.STRING,
-    })
+    }, options)
   },
 
   async down (queryInterface, Sequelize) {
+    options.tableName = "Users"
     await queryInterface.removeColumn('Users', 'firstName');
     await queryInterface.removeColumn('Users', 'lastName');
   }
