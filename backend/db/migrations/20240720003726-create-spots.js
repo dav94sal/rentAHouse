03d7eecc609b63b6a3d45bef0f444b4a1caf1e7e
david <dav94sal@gmail.com>
2024-07-21 15:27:52 -0500

    changed spot table to Spot

--- backend/db/migrations/20240720003726-create-spots.js
+++ backend/db/migrations/20240720003726-create-spots.js
@@ -8,7 +8,7 @@ if (process.env.NODE_ENV === 'production') {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable('spots', {
+    await queryInterface.createTable('Spots', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -70,6 +70,6 @@ module.exports = {
     }, options);
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('spots');
+    await queryInterface.dropTable('Spots');
   }
 };
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -3,6 +3,19 @@ const { Spot } = require('../../db/models');
 
 const router = express.Router();
 
+router.get('/:id', async (req,res,next) => {
+  let spot = await Spot.findByPk(req.params.id);
+  console.log(spot)
+  const reviews = await spot.getReviews();
+  // const avgStarRating = reviews.reduce((acc, rev) => acc + rev.stars, 0);
+
+
+  spot = spot.toJSON();
+  // spot.numReviews = numReviews;
+
+  res.json(spot)
+})
+
 router.get('/', async (req,res,next) => {
   const response = [];
   const spots = await Spot.findAll();
