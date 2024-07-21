51bc39816fd2998ce17c16150d525136decb906b
david <dav94sal@gmail.com>
2024-07-20 21:26:05 -0500

    Add get-all-spots route

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,18 +1,26 @@
 const express = require('express');
 const { Spot } = require('../../db/models');
-const { Review } = require('../../db/models');
 
 const router = express.Router();
 
 router.get('/', async (req,res,next) => {
-  const spots = await Spot.findAll({
-    include: {
-      model: Review,
-      attributes: ['stars']
-    }
-  });
+  const response = [];
+  const spots = await Spot.findAll();
 
-  res.json(spots)
+  for (let spot of spots) {
+    const images = await spot.getImages();
+    const reviews = await spot.getReviews({attributes: ['stars']});
+    const avgRating = reviews.reduce(
+      (acc, review) => acc + review.stars, 0
+    ) / reviews.length;
+
+    spot = spot.toJSON();
+    spot.avgRating = avgRating;
+    spot.previewImage = images;
+    response.push(spot)
+  }
+
+  res.json(response)
 })
 
 module.exports = router;
