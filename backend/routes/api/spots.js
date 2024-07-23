eee109a4e2ceb450d176cccb16da400d6107f369
david <dav94sal@gmail.com>
2024-07-23 15:26:19 -0500

    add endpoint to create new spot

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,7 +2,41 @@ const express = require('express');
 const { Spot, User } = require('../../db/models');
 const { requireAuth, restoreUser, decodeJWT } = require('../../utils/auth');
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
 const router = express.Router();
+
+const validateSpot = [
+  check('address')
+    .exists({checkFalsy: true})
+    .withMessage("Street address is required"),
+  check('city')
+    .exists({checkFalsy: true})
+    .withMessage("City is required"),
+  check('state')
+    .exists({checkFalsy: true})
+    .withMessage("State is required"),
+  check('country')
+    .exists({checkFalsy: true})
+    .withMessage("Country is required"),
+  check('lat')
+    .exists({checkFalsy: true})
+    .withMessage("Latitude must be within -90 and 90"),
+  check('lng')
+    .exists({checkFalsy: true})
+    .withMessage("Longitude must be within -180 and 180"),
+  check('name')
+    .exists({checkFalsy: true})
+    .withMessage("Name must be less than 50 characters"),
+  check('description')
+    .exists({checkFalsy: true})
+    .withMessage("Description is required"),
+  check('price')
+    .exists({checkFalsy: true})
+    .withMessage("Price per day must be a positive number"),
+  handleValidationErrors
+]
 
 // get all spots owned by current user
 router.get('/current', requireAuth, restoreUser, async (req,res,next) => {
@@ -78,6 +112,18 @@ router.get('/', async (req,res,next) => {
   }
 
   res.json(response)
+})
+
+router.post('/', requireAuth, restoreUser, validateSpot, async (req,res,next) => {
+  const { address, city, state, country, lat, lng, name, description, price } = req.body;
+  const JWT = decodeJWT(req);
+  const ownerId = JWT.data.id;
+
+  const newSpot = await Spot.create({
+    ownerId, address, city, state, country, lat, lng, name, description, price
+  })
+
+  res.json(newSpot);
 })
 
 module.exports = router;
