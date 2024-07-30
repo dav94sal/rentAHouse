adeb6146ca568912c2f6461c05851c15c1454ecc
david <dav94sal@gmail.com>
2024-07-30 15:16:19 -0500

    change response to match docs

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -33,7 +33,7 @@ router.get('/current', requireAuth, async (req,res,next) => {
     response.push(booking);
   }
 
-  res.json(response);
+  res.json({bookings: response});
 })
 
 module.exports = router;
