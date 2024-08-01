0f0baa304f874d7627fa8e1a5a25d4a3ba44443f
david <dav94sal@gmail.com>
2024-08-01 13:29:46 -0500

    update user error for a username / email that already exists

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -35,9 +35,30 @@ const validateSignup = [
   handleValidationErrors
 ];
 
+const userExists = async function(username, email) {
+  const usernameExists = await User.findAll({ where: { username } });
+  const emailExists = await User.findAll({ where: { email } });
+
+  if (usernameExists) {
+    return {"username": "User with that username already exists"}
+  }
+  else if (emailExists) {
+    return {"email": "User with that email already exists"}
+  } else return false;
+}
+
 // sign up
 router.post('/', validateSignup, async (req,res,next) => {
   const { username, email, password, firstName, lastName } = req.body;
+  const userExists = userExists(username, email);
+  if (userExists){
+    const err = new Error(`User already exists`);
+    err.title = 'User already exists';
+    err.errors = userExists;
+    err.status = 500;
+    next(err);
+  }
+
   const hashedPassword = bcrypt.hashSync(password);
   const user = await User.create({
     username,
