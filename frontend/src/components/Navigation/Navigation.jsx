c27387a3191b4b35e339168e865f36b6b1daaf19
david <dav94sal@gmail.com>
2024-09-02 06:01:14 -0500

    Add css styling to nav bar

--- frontend/src/components/Navigation/Navigation.jsx
+++ frontend/src/components/Navigation/Navigation.jsx
@@ -1,34 +1,24 @@
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { logout } from '../../store/session';
 import ProfileButton from './ProfileButton';
 import './Navigation.css'
 
 function Navigation() {
   const sessionUser = useSelector(state => state.session.user)
-  const dispatch = useDispatch();
 
   return (
     <nav>
       <ul>
-        <li>
+        <li id='home-link'>
           <Link to='/'>Home</Link>
         </li>
 
         {sessionUser?
-          <div>
-            <li>
-              <button
-                onClick={() => dispatch(logout())}
-              >
-                Logout
-              </button>
-            </li>
+          <li id='profile-button'>
+            <ProfileButton user={sessionUser} />
+          </li> :
 
-            <ProfileButton />
-          </div> :
-
-          <div>
+          <div id='login-signup-container'>
             <li>
               <Link to='/login'>Login</Link>
             </li>
--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,16 +1,36 @@
 import { LiaDrupal } from "react-icons/lia";
+import { useDispatch } from "react-redux";
+import { logout } from "../../store/session";
 
-function ProfileButton() {
+function ProfileButton({ user }) {
+  const dispatch = useDispatch();
+
+  const handleLogout = e => {
+    e.preventDefault();
+    dispatch(logout())
+  }
+
   return (
-    <button>
-      <ProfileIcon />
-    </button>
+    <>
+      <button>
+        <ProfileIcon />
+      </button>
+
+      <ul className="profile-dropdown">
+        <li>{user.username}</li>
+        <li>{user.firstName} {user.lastName}</li>
+        <li>{user.email}</li>
+        <li>
+          <button onClick={handleLogout}>Logout</button>
+        </li>
+      </ul>
+    </>
   )
 }
 
 function ProfileIcon() {
   return (
-    <div style={{color: "blue", fontSize: "30px"}}>
+    <div style={{color: "white", fontSize: "40px"}}>
       <LiaDrupal />
     </div>
   )
