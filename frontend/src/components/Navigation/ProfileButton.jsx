f74cc855c23934640c254fc70b04e289185c8a1e
david <dav94sal@gmail.com>
2024-09-02 16:04:48 -0500

    Add dropdown menu functionality

--- frontend/src/components/Navigation/ProfileButton.jsx
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -1,22 +1,47 @@
+import { useEffect, useState, useRef } from "react";
 import { LiaDrupal } from "react-icons/lia";
 import { useDispatch } from "react-redux";
 import { logout } from "../../store/session";
 
 function ProfileButton({ user }) {
+  const [showMenu, setShowMenu] = useState(false)
   const dispatch = useDispatch();
+  const ulref = useRef();
+
+  useEffect(() => {
+    if (!showMenu) return;
+
+    const closeMenu = (e) => {
+      if (ulref.current && !ulref.current.contains(e.target))
+      setShowMenu(false)
+    }
+
+    document.addEventListener('click', closeMenu)
+
+    return () => document.removeEventListener('click', closeMenu)
+  }, [showMenu])
 
   const handleLogout = e => {
     e.preventDefault();
     dispatch(logout())
   }
 
+  const toggleMenu = e => {
+    e.stopPropagation();
+    setShowMenu(!showMenu)
+  }
+
   return (
     <>
-      <button>
+      <button onClick={toggleMenu}>
         <ProfileIcon />
       </button>
 
-      <ul className="profile-dropdown">
+      <ul className={
+          `profile-dropdown ${showMenu? '' : 'hidden'}`
+        }
+        ref={ulref}
+      >
         <li>{user.username}</li>
         <li>{user.firstName} {user.lastName}</li>
         <li>{user.email}</li>
