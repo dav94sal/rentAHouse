722f6ccdd525be791000659f1a631f55c08b7445
david <dav94sal@gmail.com>
2024-09-09 18:57:50 -0500

    git commit testing

--- frontend/src/components/CreateSpotForm/SpotForm.jsx
+++ frontend/src/components/CreateSpotForm/SpotForm.jsx
@@ -1,9 +1,71 @@
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Navigate } from 'react-router-dom';
+import { postSpot } from '../../store/spots';
 import './SpotForm.css';
 
 function SpotForm() {
+  const [country, setCountry] = useState('');
+  const [address, setAddress] = useState('');
+  const [city, setCity] = useState('');
+  const [state, setState] = useState('');
+  const [latitude, setLatitude] = useState('');
+  const [longitude, setLongitude] = useState('');
+  const [description, setDescription] = useState('');
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [preview, setPreview] = useState('');
+  const [img2, setImg2] = useState('');
+  const [img3, setImg3] = useState('');
+  const [img4, setImg4] = useState('');
+  const [img5, setImg5] = useState('');
+  const [errors, setErrors] = useState({})
+  const dispatch = useDispatch()
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    const validations = {}
+    const images = { 1: {url: preview, preview: true} };
+    const imgArr = [img2, img3, img4, img5];
+
+    if (!preview) validations.preview = "Preview image is required";
+
+    imgArr.forEach((img, i) => {
+      if (img) images[i + 2] = {url: img, preview: false};
+    });
+
+    const spotObj = {
+      spot: {
+        country,
+        address,
+        city,
+        state,
+        lat: latitude,
+        lng: longitude,
+        name,
+        description,
+        price,
+      },
+      images
+    }
+
+    const newSpot = dispatch(postSpot(spotObj)).catch(err => validations.errors = err)
+    console.log(validations)
+
+    // if (newSpot) {
+    //   <Navigate to={`/spots/${newSpot.id}`} />
+    // }
+
+    validations[newSpot.errors] = newSpot.errors
+
+    setErrors(validations)
+  }
+
   return (
     <div className='spot-form-container'>
-      <div className='header'>
+
+      <div className='header container'>
         <h1>Create a new Spot</h1>
         <h2>Where&apos;s your place located?</h2>
         <p>
@@ -11,7 +73,7 @@ function SpotForm() {
         </p>
       </div>
 
-      <div className='location'>
+      <div className='location container'>
         <label htmlFor='country'>
           Country
         </label>
@@ -19,6 +81,8 @@ function SpotForm() {
           name='country'
           type='text'
           placeholder='Country'
+          value={country}
+          onChange={e => setCountry(e.target.value)}
         />
 
         <label>
@@ -27,6 +91,8 @@ function SpotForm() {
         <input
           type='text'
           placeholder='Address'
+          value={address}
+          onChange={e => setAddress(e.target.value)}
         />
 
         <div className='city-state'>
@@ -35,6 +101,8 @@ function SpotForm() {
             <input
               type='text'
               placeholder='City'
+              value={city}
+              onChange={e => setCity(e.target.value)}
             />
           </label>
           <p id='comma'>,</p>
@@ -44,6 +112,8 @@ function SpotForm() {
             <input
               type='text'
               placeholder='State'
+              value={state}
+              onChange={e => setState(e.target.value)}
             />
           </label>
         </div>
@@ -54,6 +124,8 @@ function SpotForm() {
             <input
               type='text'
               placeholder='Latitude'
+              value={latitude}
+              onChange={e => setLatitude(e.target.value)}
             />
           </label>
           <p id='comma'>,</p>
@@ -62,13 +134,14 @@ function SpotForm() {
             <input
               type='text'
               placeholder='Longitude'
+              value={longitude}
+              onChange={e => setLongitude(e.target.value)}
             />
           </label>
         </div>
-
       </div>
 
-      <div className='description'>
+      <div className='description container'>
         <h2>Describe your place to guests</h2>
 
         <p>
@@ -81,7 +154,95 @@ function SpotForm() {
           id='input-description'
           type='textarea'
           placeholder='Please write at least 30 characters'
+          value={description}
+          onChange={e => setDescription(e.target.value)}
         />
+      </div>
+
+      <div className='create-name container'>
+        <h2>Create a name for your spot</h2>
+
+        <p>
+          Catch guests&apos; attention with a spot name that highlights what makes
+          your place special..
+        </p>
+
+        <input
+          type='text'
+          placeholder='Name of your spot'
+          value={name}
+          onChange={e => setName(e.target.value)}
+        />
+      </div>
+
+      <div className='pricing container'>
+        <h2>Set a base price for your spot</h2>
+
+        <p>
+          Competitive pricing can help your listing stand out and rank higher
+          in search results.
+        </p>
+
+        <p>$</p>
+        <input
+          type='text'
+          placeholder='Price per night (USD)'
+          value={price}
+          onChange={e => setPrice(e.target.value)}
+        />
+      </div>
+
+      <div className='add-photos container'>
+        <h2>Liven up your spot with photos</h2>
+
+        <p>
+          Submit a link to at least one photo to publish your spot.
+        </p>
+
+        <input
+          type='text'
+          placeholder='Preview Image URL'
+          value={preview}
+          onChange={e => setPreview(e.target.value)}
+        />
+        {preview? '': errors.preview}
+
+        <input
+          type='text'
+          placeholder='Image URL'
+          value={img2}
+          onChange={e => setImg2(e.target.value)}
+        />
+
+        <input
+          type='text'
+          placeholder='Image URL'
+          value={img3}
+          onChange={e => setImg3(e.target.value)}
+        />
+
+        <input
+          type='text'
+          placeholder='Image URL'
+          value={img4}
+          onChange={e => setImg4(e.target.value)}
+        />
+
+        <input
+          type='text'
+          placeholder='Image URL'
+          value={img5}
+          onChange={e => setImg5(e.target.value)}
+        />
+      </div>
+
+      <div className='submit container'>
+        <button
+          type='submit'
+          onClick={handleSubmit}
+        >
+          Create Spot
+        </button>
       </div>
     </div>
   )
--- frontend/src/store/spots.js
+++ frontend/src/store/spots.js
@@ -1,7 +1,8 @@
-// import { csrfFetch } from "./csrf";
+import { csrfFetch } from "./csrf";
 
 const POPULATE = 'spot/populate';
 const DETAILS = 'spot/details';
+const ADD_ONE = 'spot/add_one';
 
 // action creators
 export const populateSpots = (allSpots) => {
@@ -15,6 +16,13 @@ export const spotDetails = (spot) => {
   return {
     type: DETAILS,
     spot
+  }
+}
+
+export const addSpot = (newSpot) => {
+  return {
+    type: ADD_ONE,
+    newSpot
   }
 }
 
@@ -38,6 +46,28 @@ export const getSpotDetails = (spotId) => async dispatch => {
   }
 }
 
+export const postSpot = (spot) => async dispatch => {
+  const response = await csrfFetch('/api/spots', {
+    method: 'POST',
+    body: JSON.stringify(spot.spot)
+  }).catch()
+
+  const newSpot = await response.json();
+  if (response.ok) {
+    dispatch(addSpot(newSpot));
+
+    const images = Object.values(spot.images)
+    images.forEach(async img => {
+      await csrfFetch(`/api/spots/${newSpot.id}/images`, {
+        method: 'POST',
+        body: { img }
+      })
+    });
+
+  }
+  return newSpot;
+}
+
 const initialState = {};
 
 export default function spotReducer(state = initialState, action) {
@@ -52,6 +82,11 @@ export default function spotReducer(state = initialState, action) {
     case DETAILS: {
       const newState = {...state};
       newState[action.spot.id] = action.spot;
+      return newState;
+    } // Combine DETAILS and ADD_ONE if testing goes well
+    case ADD_ONE: {
+      const newState = {...state};
+      newState[action.newSpot.id] = action.newSpot;
       return newState;
     }
     default:
